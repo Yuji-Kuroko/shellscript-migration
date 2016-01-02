78a7ef541932ce7b676f3094c125ef9c1e2d8545
Yuji-Kuroko <patchouli.royalflare+git@gmail.com>
2016-01-02 19:29:43 +0900

    add "migrate" function

--- mg.sh
+++ mg.sh
@@ -1,11 +1,36 @@
 #!/bin/sh
 
 migrate() {
-  if [ $# -eq 1 ]; then
-    echo "Usage: migrate"
+  if [ $# -ne 1 ]; then
+    echo "Usage: $0 migrate"
     exit 1
   fi
 
+  local mg_dir="migrations"
+  local mg_data_file="tmp/migrated.dat"
+  local mg_data_tmp_file="tmp/tmp.migrated.dat"
+  local mg_migrating_file="tmp/migrating.dat"
+  local mg_migration_log="tmp/migration.log"
+
+  if [ ! -f $mg_data_file ]; then
+    touch $mg_data_file
+  fi
+
+  touch $mg_migrating_file
+  for script_file in $(ls $mg_dir | comm -23 - $mg_data_file)
+  do
+    echo $script_file | tee -a $mg_migrating_file
+    . ${mg_dir}/${script_file}
+    echo $(date +'%Y-%m-%d %H:%M:%S'),${script_file} >> $mg_migration_log
+  done
+
+  # update migrated log
+  cp -f $mg_data_file $mg_data_tmp_file
+  cat $mg_migrating_file >> $mg_data_tmp_file
+  sort $mg_data_tmp_file -o $mg_data_file
+
+  rm $mg_data_tmp_file
+  rm $mg_migrating_file
 }
 
 create() {
